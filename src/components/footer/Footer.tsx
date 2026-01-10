49feebb5bd382f5f2b327b9455e849b06ce54952
Juan <juanpablolamas01@gmail.com>
2026-01-09 22:40:55 -0300

    Gravia - Website 0.1 - 2240-fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// Image imports
-import locationIcon from "../../images/icon/location-icon.svg";
-import callIcon from "../../images/icon/call-icon.svg";
-
 const Footer: React.FC = () => {
   return (
     <footer
@@ -12,25 +8,11 @@ const Footer: React.FC = () => {
     >
       <div className="xb-footer-wrap">
         {/* Footer Bottom */}
-        <div className="xb-footer-bottom">
-          <div className="contact-item">
-            <img src={locationIcon} alt="location" />
-            <span className="contact-method">Maldonado 1565, Montevideo, Uruguay</span>
-          </div>
-
+        <div className="xb-footer-bottom xb-footer-bottom--center">
           <div className="contact-item copyright-item">
             <p>
-              Copyright © 2025 <Link to="/">Gravia</Link>, All rights reserved.
+              Copyright © 2026 <Link to="/">Gravia</Link>, All rights reserved.
             </p>
-          </div>
-
-          <div className="contact-item">
-            <a href="tel:+112304528597">
-              <img src={callIcon} alt="call" />
-            </a>
-            <a className="contact-method" href="tel:+112304528597">
-              +(598) 95 855 999
-            </a>
           </div>
         </div>
       </div>
